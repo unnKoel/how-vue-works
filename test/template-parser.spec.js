394b1659115feadce916f8171e66ae896db15c6b
unnkoel <unnkoel@gmail.com>
2022-12-20 04:10:15 +0800

    test cascade tags

--- test/template-parser.spec.js
+++ test/template-parser.spec.js
@@ -62,9 +62,13 @@ test('abstract "br" tag from html template', () => {
 test('parse templete to become an object tree', () => {
   const objectTree = parse(`
   <div class="right">
-    <h2 class="vue">Vue.js <br /> is an progressive framework.</h2>
-    <h2 class="vue">Vue.js <br/> is an progressive framework.</h2>
-    <h2 class="vue">Vue.js <br/> is an progressive framework.</h2>
+    <div class="block1">
+      <div class="block2" id="youth">
+        <h2 class="vue">Vue.js <br /> is an progressive framework.</h2>
+        <h2 class="vue">Vue.js <br/> is an progressive framework.</h2>
+        <h2 class="vue">Vue.js <br/> is an progressive framework.</h2>
+      </div>
+    </div>
   </div>`);
 
   expect(objectTree).toStrictEqual({
@@ -72,43 +76,55 @@ test('parse templete to become an object tree', () => {
     attributes: { class: 'right' },
     children: [
       {
-        tag: 'h2',
-        attributes: { class: 'vue' },
+        tag: 'div',
+        attributes: { class: 'block1' },
         children: [
-          'Vue.js',
           {
-            tag: 'br',
-            attributes: {},
-            children: [],
-          },
-          'is an progressive framework.'
-        ],
-      },
-      {
-        tag: 'h2',
-        attributes: { class: 'vue' },
-        children: [
-          'Vue.js',
-          {
-            tag: 'br',
-            attributes: {},
-            children: [],
-          },
-          'is an progressive framework.'
-        ],
-      },
-      {
-        tag: 'h2',
-        attributes: { class: 'vue' },
-        children: [
-          'Vue.js',
-          {
-            tag: 'br',
-            attributes: {},
-            children: [],
-          },
-          'is an progressive framework.'
-        ],
+            tag: 'div',
+            attributes: { class: 'block2', id: 'youth' },
+            children: [
+              {
+                tag: 'h2',
+                attributes: { class: 'vue' },
+                children: [
+                  'Vue.js',
+                  {
+                    tag: 'br',
+                    attributes: {},
+                    children: [],
+                  },
+                  'is an progressive framework.'
+                ],
+              },
+              {
+                tag: 'h2',
+                attributes: { class: 'vue' },
+                children: [
+                  'Vue.js',
+                  {
+                    tag: 'br',
+                    attributes: {},
+                    children: [],
+                  },
+                  'is an progressive framework.'
+                ],
+              },
+              {
+                tag: 'h2',
+                attributes: { class: 'vue' },
+                children: [
+                  'Vue.js',
+                  {
+                    tag: 'br',
+                    attributes: {},
+                    children: [],
+                  },
+                  'is an progressive framework.'
+                ],
+              }
+            ]
+          }
+        ]
       }
     ],
   });
