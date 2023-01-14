37dc8ab443a4e9202dcc8fa7fcf825419b76c9b0
unnkoel <unnkoel@gmail.com>
2023-01-14 09:12:08 +0800

    override array original functions to trigger execution of wather list

--- src/observe.js
+++ src/observe.js
@@ -3,13 +3,45 @@
  *  create observer for data to watch its change, by defining its set 
  *  and get function by `Object.defineProperty`.
  *  
- *  @todo need to support array type.
+ *  @todo how to reuse dom while array has been sorted or reversed.
  */
 
+const ARRAY_REDEFINE_FUNS = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+
 const attacheWathers = (value, watchers) => {
+  if (typeof value === 'object') {
+    return Object.defineProperty(value, 'watchers', {
+      value: watchers
+    });
+  }
+
   const protoType = Object.getPrototypeOf(value);
   protoType.watchers = watchers;
 }
+
+const wrapperReactiveArray = (value, watchers) => {
+  const arrayPrototype = Object.getPrototypeOf(value);
+  ARRAY_REDEFINE_FUNS.forEach(funcName => {
+    Object.defineProperty(value, funcName, {
+      value: function (...args) {
+        if (funcName === 'push' || funcName === 'unshift') {
+          const addedItems = args;
+          addedItems.forEach(addedItem => observe(addedItem));
+        }
+
+        const result = arrayPrototype[funcName].bind(value)(...args);
+        watchers.forEach(watcher => watcher(value));
+        return result;
+      },
+      configurable: true,
+    });
+  })
+}
+
+// const convertPrimitiveToObject = (value) => {
+//   if(typeof value === 'object') return value;
+//   return Object.prototype.valueOf.call(value);
+// }
 
 const WatcherWrapper = (initialValue) => {
   const watchers = [];
@@ -17,6 +49,10 @@ const WatcherWrapper = (initialValue) => {
 
   const get = () => {
     attacheWathers(value, watchers);
+    if (Array.isArray(value)) {
+      wrapperReactiveArray(value, watchers)
+    }
+
     return value;
   }
 
@@ -34,15 +70,23 @@ const WatcherWrapper = (initialValue) => {
 }
 
 const observe = (data = {}) => {
+  if (typeof data !== 'object') return;
+
   Object.keys(data).forEach(key => {
     const value = data[key];
 
     Object.defineProperty(data, key, WatcherWrapper(value));
 
     if (typeof value === 'object' && value !== null) {
+      if (Array.isArray(value)) {
+        return value.forEach(item => observe(item));
+      }
+
       return observe(value);
     }
   });
+
+  return data;
 };
 
 export default observe;
--- test/observe.spec.js
+++ test/observe.spec.js
@@ -37,8 +37,25 @@ test('test wather\'s effect for multiple layers of data', () => {
   expect(data.person.tech).toBe('react');
 });
 
-// test('test wather\'s effect for array type of data', () => {
-//   const data = { person: [{ name: 'addy', tech: 'vue' }] };
-//   observe(data);
-  
-// });
+test('test wather\'s effect for array type of data', () => {
+  const data = {
+    persons: [{ name: 'addy', tech: 'vue' }],
+    groups: [1, 2, 3]
+  };
+  observe(data);
+
+  data.persons.watchers.push((data) => {
+    expect(data).toStrictEqual([{ name: 'addy', tech: 'vue' }, { name: 'addy1', tech: 'react' }]);
+  });
+
+  data.groups.watchers.push((data) => {
+    expect(data).toStrictEqual([1, 2, 3, 4]);
+  });
+
+  const persons = data.persons;
+  const groups = data.groups;
+  persons.push({ name: 'addy1', tech: 'react' });
+  groups.push(4);
+  expect(groups).toStrictEqual([1, 2, 3, 4]);
+  expect(persons).toStrictEqual([{ name: 'addy', tech: 'vue' }, { name: 'addy1', tech: 'react' }]);
+});
