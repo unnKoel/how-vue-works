5dd71a47e6de2ce4ec3d4378df253d2a7cecdbb8
unnkoel <unnkoel@gmail.com>
2023-01-15 05:34:15 +0800

    replace directly get watchers as a list with watch function

--- src/observe.js
+++ src/observe.js
@@ -1,42 +1,57 @@
-
 /**
- *  create observer for data to watch its change, by defining its set 
+ *  create observer for data to watch its change, by defining its set
  *  and get function by `Object.defineProperty`.
- *  
+ *
  *  @todo how to reuse dom while array has been sorted or reversed.
  */
 
-const ARRAY_REDEFINE_FUNS = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+const ARRAY_REDEFINE_FUNS = [
+  "push",
+  "pop",
+  "shift",
+  "unshift",
+  "splice",
+  "sort",
+  "reverse",
+];
 
 const attacheWathers = (value, watchers) => {
-  if (typeof value === 'object') {
-    return Object.defineProperty(value, 'watchers', {
-      value: watchers
+  if (typeof value === "object") {
+    return Object.defineProperty(value, "watch", {
+      value: function (watcher) {
+        watchers.push(watcher);
+      },
+      configurable: true,
     });
   }
 
   const protoType = Object.getPrototypeOf(value);
-  protoType.watchers = watchers;
-}
+  return Object.defineProperty(protoType, "watch", {
+    value: function (watcher) {
+      watchers.push(watcher);
+    },
+    configurable: true,
+  });
+};
 
 const wrapperReactiveArray = (value, watchers) => {
   const arrayPrototype = Object.getPrototypeOf(value);
-  ARRAY_REDEFINE_FUNS.forEach(funcName => {
+  ARRAY_REDEFINE_FUNS.forEach((funcName) => {
     Object.defineProperty(value, funcName, {
       value: function (...args) {
-        if (funcName === 'push' || funcName === 'unshift') {
+        if (funcName === "push" || funcName === "unshift") {
           const addedItems = args;
-          addedItems.forEach(addedItem => observe(addedItem));
+          addedItems.forEach((addedItem) => observe(addedItem));
         }
 
         const result = arrayPrototype[funcName].bind(value)(...args);
-        watchers.forEach(watcher => watcher(value));
+        watchers.forEach((watcher) => watcher(value));
         return result;
       },
       configurable: true,
     });
-  })
-}
+  });
+};
 
 // const convertPrimitiveToObject = (value) => {
 //   if(typeof value === 'object') return value;
@@ -50,36 +65,36 @@ const WatcherWrapper = (initialValue) => {
   const get = () => {
     attacheWathers(value, watchers);
     if (Array.isArray(value)) {
-      wrapperReactiveArray(value, watchers)
+      wrapperReactiveArray(value, watchers);
     }
 
     return value;
-  }
+  };
 
   const set = (newValue) => {
     if (value !== newValue) {
       value = newValue;
-      watchers.forEach(watcher => watcher(value));
+      watchers.forEach((watcher) => watcher(value));
     }
-  }
+  };
 
   return {
     get,
     set,
-  }
-}
+  };
+};
 
 const observe = (data = {}) => {
-  if (typeof data !== 'object') return;
+  if (typeof data !== "object") return;
 
-  Object.keys(data).forEach(key => {
+  Object.keys(data).forEach((key) => {
     const value = data[key];
 
     Object.defineProperty(data, key, WatcherWrapper(value));
 
-    if (typeof value === 'object' && value !== null) {
+    if (typeof value === "object" && value !== null) {
       if (Array.isArray(value)) {
-        return value.forEach(item => observe(item));
+        return value.forEach((item) => observe(item));
       }
 
       return observe(value);
--- test/observe.spec.js
+++ test/observe.spec.js
@@ -1,61 +1,67 @@
 /* eslint-disable no-undef */
 import observe from "../src/observe";
 
-test('test wather\'s effect for only single layer of data', () => {
-  const data = { name: 'addy' };
+test("test wather's effect for only single layer of data", () => {
+  const data = { name: "addy" };
   observe(data);
 
-  expect(data.name).toStrictEqual('addy');
+  expect(data.name).toStrictEqual("addy");
 
-  data.name.watchers.push((data) => {
-    expect(data).toBe('YanJunZhou');
+  data.name.watch((data) => {
+    expect(data).toBe("YanJunZhou");
   });
 
-  data.name = 'YanJunZhou';
-  expect(data.name).toBe('YanJunZhou');
+  data.name = "YanJunZhou";
+  expect(data.name).toBe("YanJunZhou");
 });
 
-test('test wather\'s effect for multiple layers of data', () => {
-  const data = { person: { name: 'addy', tech: 'vue' } };
+test("test wather's effect for multiple layers of data", () => {
+  const data = { person: { name: "addy", tech: "vue" } };
   observe(data);
 
-  expect(data.person.name).toBe('addy');
+  expect(data.person.name).toBe("addy");
 
-  expect(data.person.tech).toBe('vue');
+  expect(data.person.tech).toBe("vue");
 
-  data.person.name.watchers.push((data) => {
-    expect(data).toBe('YanJunZhou');
+  data.person.name.watch((data) => {
+    expect(data).toBe("YanJunZhou");
   });
 
-  data.person.tech.watchers.push((data) => {
-    expect(data).toBe('react');
+  data.person.tech.watch((data) => {
+    expect(data).toBe("react");
   });
 
-  data.person.name = 'YanJunZhou';
-  expect(data.person.name).toBe('YanJunZhou');
-  data.person.tech = 'react';
-  expect(data.person.tech).toBe('react');
+  data.person.name = "YanJunZhou";
+  expect(data.person.name).toBe("YanJunZhou");
+  data.person.tech = "react";
+  expect(data.person.tech).toBe("react");
 });
 
-test('test wather\'s effect for array type of data', () => {
+test("test wather's effect for array type of data", () => {
   const data = {
-    persons: [{ name: 'addy', tech: 'vue' }],
-    groups: [1, 2, 3]
+    persons: [{ name: "addy", tech: "vue" }],
+    groups: [1, 2, 3],
   };
   observe(data);
 
-  data.persons.watchers.push((data) => {
-    expect(data).toStrictEqual([{ name: 'addy', tech: 'vue' }, { name: 'addy1', tech: 'react' }]);
+  data.persons.watch((data) => {
+    expect(data).toStrictEqual([
+      { name: "addy", tech: "vue" },
+      { name: "addy1", tech: "react" },
+    ]);
   });
 
-  data.groups.watchers.push((data) => {
+  data.groups.watch((data) => {
     expect(data).toStrictEqual([1, 2, 3, 4]);
   });
 
   const persons = data.persons;
   const groups = data.groups;
-  persons.push({ name: 'addy1', tech: 'react' });
+  persons.push({ name: "addy1", tech: "react" });
   groups.push(4);
   expect(groups).toStrictEqual([1, 2, 3, 4]);
-  expect(persons).toStrictEqual([{ name: 'addy', tech: 'vue' }, { name: 'addy1', tech: 'react' }]);
+  expect(persons).toStrictEqual([
+    { name: "addy", tech: "vue" },
+    { name: "addy1", tech: "react" },
+  ]);
 });
